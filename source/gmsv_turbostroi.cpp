b08ccec00cf57af4cabee7acc4112d877693acce
kosmik641 <minecraftgame2002@gmail.com>
2026-05-01 02:58:34 +0300

    Remove garbage, rollback garrysmod_common submodule

--- source/gmsv_turbostroi.cpp
+++ source/gmsv_turbostroi.cpp
@@ -25,7 +25,7 @@ namespace GM = GarrysMod::Lua;
 //------------------------------------------------------------------------------
 std::string g_LibraryFileName;
 bool g_ForceThreadsFinished = false; // For correct unrequire module
-std::atomic<double> g_CurrentTime = 0.0f;
+std::atomic<double> g_CurrentTime = 0.0;
 unsigned int g_ThreadTickrate = 10000; // [mcs] (10ms)
 std::vector<TTrainSystem> g_MetrostroiSystemList;
 std::queue<TTrainSystem> g_LoadSystemList;
@@ -334,7 +334,7 @@ LUA_FUNCTION( API_StartRailNetwork )
 //------------------------------------------------------------------------------
 // SourceSDK
 //------------------------------------------------------------------------------
-DLL_EXPORT LUA_FUNCTION_DECLARE( Think_handler )
+LUA_FUNCTION_DECLARE( Think_handler )
 {
 	g_CurrentTime = *(double*)((char*)g_pServerGlobalVars + 12);
 	g_SharedPrint.PrintAvailable();
